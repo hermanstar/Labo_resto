20014d85d5d6b10a4f35d3610a53fdb30686e75c
121790 <joel.kanme@gmail.com>
2020-06-22 00:52:59 +0200

    suites de l'update des URL

--- restaurant/views.py
+++ restaurant/views.py
@@ -4,13 +4,23 @@ from .models import Menu
 
 def home(request):
     menus = Menu.objects.all()
-    return render(request, 'home.html', {'menus': menus})
+    context = {
+        'menus': menus ,
+    }
+    return render(request, 'home.html', context)
 
 
 def menus_produit(request, slug):
     menu =  Menu.objects.get(slug=slug)
     menus = Menu.objects.all()
-    return render(request, 'produits.html', {'menu': menu, 'menus': menus})
+    
+    context  = {
+        'menus': menus ,
+        'menu' : menu ,  
+    }
+    
+    return render(request, 'produits.html', context) 
+
 
 
 def resumer_commande():
